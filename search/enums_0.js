023a527d28891425bce9948390b4c840e5335c9c
olilarkin <olilarkin@users.noreply.github.com>
2025-03-10 22:11:38 +0000

    deploy: iPlug2/iPlug2@d0b1e8f6b6438cc4e873c07ff9b3e0962682f660

--- search/enums_0.js
+++ search/enums_0.js
@@ -10,5 +10,6 @@ var searchData=
   ['eparamunit_7',['EParamUnit',['../class_i_param.html#a875d89d1ca38c32a58c1c9c00dcacd3d',1,'IParam']]],
   ['epopupstate_8',['EPopupState',['../class_i_bubble_control.html#a70b7ab408a4856065d59781e94640190',1,'IBubbleControl::EPopupState()'],['../class_i_popup_menu_control.html#a8e714bc93d86a7c5d80a117e3e48c8c6',1,'IPopupMenuControl::EPopupState()']]],
   ['eroute_9',['ERoute',['../group___i_plug_constants.html#ga2d2950b86093091e6a4fcf3fa7c6732a',1,'IPlugConstants.h']]],
-  ['estatusmsg_10',['EStatusMsg',['../struct_i_midi_msg.html#a716bcebce861a17cebe33910e7285e1a',1,'IMidiMsg']]]
+  ['eshapeids_10',['EShapeIDs',['../class_i_param.html#a6857072493bf8c971efa7204f0c217c6',1,'IParam']]],
+  ['estatusmsg_11',['EStatusMsg',['../struct_i_midi_msg.html#a716bcebce861a17cebe33910e7285e1a',1,'IMidiMsg']]]
 ];
