3d5e2c4f2515cb399bc30c0bc6c9818e0f8ce83c
olilarkin <olilarkin@users.noreply.github.com>
2025-05-03 11:35:12 +0000

    deploy: iPlug2/iPlug2@dcf40434d54c968897618f6fcdff1843d624ccce

--- search/classes_f.js
+++ search/classes_f.js
@@ -1,12 +1,13 @@
 var searchData=
 [
-  ['shape_0',['Shape',['../struct_i_param_1_1_shape.html',1,'IParam']]],
-  ['shapeexp_1',['ShapeExp',['../struct_i_param_1_1_shape_exp.html',1,'IParam']]],
-  ['shapelinear_2',['ShapeLinear',['../struct_i_param_1_1_shape_linear.html',1,'IParam']]],
-  ['shapepowcurve_3',['ShapePowCurve',['../struct_i_param_1_1_shape_pow_curve.html',1,'IParam']]],
-  ['sinoscillator_4',['SinOscillator',['../class_sin_oscillator.html',1,'']]],
-  ['smoothedgain_5',['SmoothedGain',['../class_smoothed_gain.html',1,'']]],
-  ['svf_6',['SVF',['../class_s_v_f.html',1,'']]],
-  ['synthvoice_7',['SynthVoice',['../class_synth_voice.html',1,'']]],
-  ['sysexdata_8',['SysExData',['../struct_sys_ex_data.html',1,'']]]
+  ['scopedglcontext_0',['ScopedGLContext',['../class_i_graphics_1_1_scoped_g_l_context.html',1,'IGraphics']]],
+  ['shape_1',['Shape',['../struct_i_param_1_1_shape.html',1,'IParam']]],
+  ['shapeexp_2',['ShapeExp',['../struct_i_param_1_1_shape_exp.html',1,'IParam']]],
+  ['shapelinear_3',['ShapeLinear',['../struct_i_param_1_1_shape_linear.html',1,'IParam']]],
+  ['shapepowcurve_4',['ShapePowCurve',['../struct_i_param_1_1_shape_pow_curve.html',1,'IParam']]],
+  ['sinoscillator_5',['SinOscillator',['../class_sin_oscillator.html',1,'']]],
+  ['smoothedgain_6',['SmoothedGain',['../class_smoothed_gain.html',1,'']]],
+  ['svf_7',['SVF',['../class_s_v_f.html',1,'']]],
+  ['synthvoice_8',['SynthVoice',['../class_synth_voice.html',1,'']]],
+  ['sysexdata_9',['SysExData',['../struct_sys_ex_data.html',1,'']]]
 ];
