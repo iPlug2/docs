544f7b21e1faf2ddef9692a3ac84190b2d9b58f2
olilarkin <olilarkin@users.noreply.github.com>
2025-05-29 15:09:28 +0000

    deploy: iPlug2/iPlug2@37355493bbc1b03774f32ef2a534ccc221a182a2

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['bitmap_0',['Bitmap',['../class_i_graphics_canvas_1_1_bitmap.html',1,'IGraphicsCanvas::Bitmap'],['../class_i_graphics_nano_v_g_1_1_bitmap.html',1,'IGraphicsNanoVG::Bitmap'],['../class_i_graphics_skia_1_1_bitmap.html',1,'IGraphicsSkia::Bitmap']]]
+  ['bitmap_0',['Bitmap',['../class_i_graphics_nano_v_g_1_1_bitmap.html',1,'IGraphicsNanoVG::Bitmap'],['../class_i_graphics_skia_1_1_bitmap.html',1,'IGraphicsSkia::Bitmap']]]
 ];
