41059984a35848bbb17995add3a9c4a151460642
olilarkin <olilarkin@users.noreply.github.com>
2026-01-01 18:35:13 +0000

    deploy: iPlug2/iPlug2@d30d05e7b4c512227402328912b0d1a1ac28540f

--- search/pages_2.js
+++ search/pages_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['code_20style_0',['Code Style',['../md_codingstyle.html',1,'']]]
+  ['ci_20commands_20for_20pull_20requests_0',['CI Commands for Pull Requests',['../md_ci_commands.html',1,'']]],
+  ['code_20style_1',['Code Style',['../md_codingstyle.html',1,'']]]
 ];
