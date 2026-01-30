7e8ee65d52a861e9c136349064cf002886a81cd8
olilarkin <olilarkin@users.noreply.github.com>
2026-01-30 18:18:23 +0000

    deploy: iPlug2/iPlug2@da6112de020bab44e83ef3081648eeb66b94f6d5

--- search/pages_1.js
+++ search/pages_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['building_20iplug2_20projects_20with_20cmake_0',['Building iPlug2 Projects with CMake',['../md_cmake.html',1,'']]]
+  ['building_20iplug2_20projects_20with_20cmake_0',['Building iPlug2 Projects with CMake',['../md_cmake.html',1,'']]],
+  ['building_20wasm_20plugins_1',['Building WASM Plugins',['../md_wasm.html',1,'']]]
 ];
