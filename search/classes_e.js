49ec1fe14313d1c55f535320b57b523b81e333b0
olilarkin <olilarkin@users.noreply.github.com>
2026-02-12 23:53:24 +0000

    deploy: iPlug2/iPlug2@3b32d40f2b2da1f64ce2e5a6552f5bde7c76f2b1

--- search/classes_e.js
+++ search/classes_e.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['realtimeresampler_0',['RealtimeResampler',['../class_realtime_resampler.html',1,'']]],
   ['reaperaction_1',['ReaperAction',['../struct_reaper_action.html',1,'']]],
-  ['reaperextbase_2',['ReaperExtBase',['../class_reaper_ext_base.html',1,'']]]
+  ['reaperextbase_2',['ReaperExtBase',['../class_reaper_ext_base.html',1,'']]],
+  ['reaperextdockstate_3',['ReaperExtDockState',['../_reaper_ext_base_8h.html#struct_reaper_ext_dock_state',1,'']]]
 ];
